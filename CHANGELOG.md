8eeeeae8bba912adbe31305280858c0eec66e60b
lzh <1216083447@qq.com>
2025-06-30 16:53:35 +0800

    升级ionicons、fabricjs，以及一些dev依赖

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,3 +4,5 @@ All notable changes to this project will be documented in this file.
 ## [Unreleased]
 ### Changed
 - Upgraded Angular to v20.0.5
+- Upgraded fabric to v6.7.0
+- Upgraded ionicons to v8.0.9
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,16 +2,38 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
 import { Path } from '../type/enum/path';
+import {
+  starOutline,
+  playForwardOutline,
+  star,
+  bulbOutline,
+  serverOutline,
+  trashOutline,
+  personOutline,
+  chatboxEllipsesOutline,
+} from 'ionicons/icons';
+import { addIcons } from 'ionicons';
 
 @Component({
-    selector: 'app-home',
-    templateUrl: 'home.page.html',
-    styleUrls: ['home.page.scss'],
-    standalone: false
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: false,
 })
 export class HomePage {
   path = Path;
-  constructor(private router: Router, public authService: AuthService) {}
+  constructor(private router: Router, public authService: AuthService) {
+    addIcons({
+      starOutline,
+      playForwardOutline,
+      star,
+      bulbOutline,
+      serverOutline,
+      trashOutline,
+      personOutline,
+      chatboxEllipsesOutline,
+    });
+  }
   navigate(url: string) {
     this.router.navigateByUrl(url);
   }
