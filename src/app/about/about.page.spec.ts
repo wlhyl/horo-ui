14b7187dfe37e4a294422b7f84a74c74fa41625b
lzh <1216083447@qq.com>
2025-09-05 04:14:36 +0800

    为 about/about.page.ts 编写单元测试

--- src/app/about/about.page.spec.ts
+++ src/app/about/about.page.spec.ts
@@ -1,11 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Title } from '@angular/platform-browser';
+import { IonicModule } from '@ionic/angular';
 import { AboutPage } from './about.page';
 
 describe('AboutPage', () => {
   let component: AboutPage;
   let fixture: ComponentFixture<AboutPage>;
+  let titleServiceSpy: jasmine.SpyObj<Title>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    titleServiceSpy = jasmine.createSpyObj('Title', ['setTitle']);
+
+    await TestBed.configureTestingModule({
+      declarations: [AboutPage],
+      imports: [IonicModule.forRoot()],
+      providers: [{ provide: Title, useValue: titleServiceSpy }],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(AboutPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -13,5 +24,11 @@ describe('AboutPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call titleService.setTitle with correct title', () => {
+    titleServiceSpy.setTitle.calls.reset();
+    component.ngOnInit();
+    expect(titleServiceSpy.setTitle).toHaveBeenCalledWith('说明');
   });
 });
